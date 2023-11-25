a5c1575ad3158243099c1b51509e3b70001f9e4c
Jiya-dedhia-whj <94034939+Jiya-dedhia-whj@users.noreply.github.com>
2023-11-25 20:35:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function setup()
 {
@@ -24,5 +25,42 @@ function modelLoaded()
 
 function draw()
 {
-    image(video,0,0,380,280)
+    image(video,0,0,380,280);
+    if(status != "")
+    {
+        objectDetector.detect(video,gotResults);
+        for(i = 0 ; i < objects.length ; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : detecting objects";
+            console.log(objects.length);
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%",objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(object[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(object[i].label == input_text)
+            {
+                video.stop();
+                objectDetector.detect(gotResults);
+                document.getElementById("object_found").innerHTML = input_text + " Found";
+                var synth = video.speechSynthesis;
+                var utterThis = new SpeechSynthesisUtterance(input_text + "found");
+                synth.speak(utterThis)
+            }
+            else
+            {
+                document.getElementById("object_found").innerHTML = input_text + " Not Found";
+            }
+        }
+    }
+}
+
+function gotResults(results,error)
+{
+    if(results)
+    {
+        console.log(results)
+    }
+    console.log(error);
 }
